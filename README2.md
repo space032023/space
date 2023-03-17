65b2ecb4ac8f4a18aee63758d29ceffdc8e3d895
space032023 <128189646+space032023@users.noreply.github.com>
2023-03-18 05:29:18 +0800

    Add presentation files

---
+++ README2.md
@@ -0,0 +1,7 @@
+# HTML and CSS Presentation Demo with WebSlides
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/SitePoint/pen/KvmZJe](https://codepen.io/SitePoint/pen/KvmZJe).
+
+This demo by Ivaylo Gerchev accompanies an article for SitePoint and illustrates how to create an HTML and CSS presentation about SVG using WebSlides.
+Article Title: How to Create Beautiful HTML and CSS Presentations with WebSlides 
+Article URL: https://www.sitepoint.com/how-to-create-beautiful-html-css-presentations-with-webslides
---
+++ index.js
@@ -0,0 +1,106 @@
+let section = document.querySelectorAll("section");
+let menu = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  section.forEach((i) => {
+    let top = window.scrollY;
+    let offset = i.offsetTop - 150;
+    let height = i.offsetHeight;
+    let id = i.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      menu.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+
+reveal();
+
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+
+var lastId,
+ topMenu = $(".nav-item"),
+ topMenuHeight = topMenu.outerHeight()+1,
+ 
+ menuItems = topMenu.find("a"),
+
+ scrollItems = menuItems.map(function(){
+   var item = $($(this).attr("href"));
+    if (item.length) { return item; }
+ });
+
+
+menuItems.click(function(e){
+  var href = $(this).attr("href"),
+      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+  $('html, body').stop().animate({ 
+      scrollTop: offsetTop
+  }, 850);
+  e.preventDefault();
+});
+
+
+$(window).scroll(function(){
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+       lastId = id;
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href=#"+id+"]").parent().addClass("active");
+   }                   
+});
+
+
+$(document).ready(function () {
+  $(".dropdown").hover(
+    function () {
+      $(this).addClass("show");
+      $(this).find(".dropdown-menu").addClass("show");
+    },
+    function () {
+      $(this).removeClass("show");
+      $(this).find(".dropdown-menu").removeClass("show");
+    }
+  );
+});
